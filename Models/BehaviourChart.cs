7da2221d647872e2bd7b261226d5126112bee1d7
Al Morton <morton_al@hotmail.com>
2019-09-27 11:55:06 +0900

    Added some tests for the IComparable stuff

--- Models/BehaviourChart.cs
+++ Models/BehaviourChart.cs
@@ -1,13 +1,51 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BlazorApp.Models
 {
-    public class BehaviourChart
+    public class BehaviourChart 
     {
-        
+        public Status Status { get; set; }
+        public SortedList<RollRange, string> Actions { get; set; }
+
+        public BehaviourChart()
+        {
+            Actions = new SortedList<RollRange, string>();
+        }
+
+        public void GetStatus(int diceRoll)
+        {
+           
+        }
+    }
+
+    public class RollRange : IComparer<RollRange>, IComparable<RollRange>
+    {
+        public int From { get; set; }
+        public int To { get; set; }
+        public string Result { get; set; }
+
+        public int Compare(RollRange x, RollRange y)
+        {
+            if (x.From > y.From) return 1;
+            if (x.From < y.From) return -1;
+            else return 0;            
+        }
+
+        public int CompareTo(RollRange other)
+        {
+            // Falls between
+            if (other.From >= From && To <= other.To) return 1;
+
+            if (From > other.From) return 1;
+
+            if (From < other.From) return 0;
+
+            else return -1;
+        }
     }
 
     public enum Status
