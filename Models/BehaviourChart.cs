2ab68788e510be721f254728c7f2731986af3a25
Al Morton <morton_al@hotmail.com>
2019-09-27 10:26:28 +0900

    Add project files.

---
+++ Models/BehaviourChart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Models
+{
+    public class BehaviourChart
+    {
+
+    }
+
+    public enum Status
+    {
+        Hidden,
+        Engaged,
+        InCover,
+        Close,
+        Other
+    }
+
+    public static class Actions
+    {
+        public static string Hold { get; } = "Hold";
+        public static string Sneak { get; set; } = "Sneak";
+        public static string Advance { get; set; } = "Advance";
+        public static string Charge { get; set; } = "Charge";
+        public static string FallBack { get; set; } = "FallBack";
+        public static string Aim { get; set; } = "Aim";
+        public static string Onslaught { get; set; } = "Onslaught";
+    }    
+    
+}
---
+++ Pages/CounterPageComponent.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Blazor;
+using Microsoft.AspNetCore.Components;
+using System.Timers;
+
+namespace BlazorApp.Pages
+{
+    public class CounterPageComponent : ComponentBase
+    {
+        public int currentCount = 0;
+
+        public List<NameForm> Names = new List<NameForm>
+        {
+            new NameForm("Alice"),
+            new NameForm("Bob"),
+            new NameForm("John"),
+            new NameForm("Gilbert"),
+            new NameForm("Sullivan")
+        };
+
+        public string Message { get; set; }
+
+        public CounterPageComponent()
+        {
+            
+        }
+
+        public void UpdateView()
+        {
+            this.StateHasChanged();
+        }
+        public void IncrementCount()
+        {
+            currentCount++;
+        }     
+       
+    }
+
+    public class NameForm
+    {
+        public string Name { get; set; }
+
+        public bool Attending { get; set; }
+
+        public NameForm(string name)
+        {
+            Name = name;
+        }
+    }
+}
