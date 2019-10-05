8046cbfdee2de7f8ce70229d8b88273ece40f4be
Al Morton <morton_al@hotmail.com>
2019-10-05 22:02:15 +0900

    Some more changes

--- Components/InitiativeTrackComponent.cs
+++ Components/InitiativeTrackComponent.cs
@@ -11,53 +11,51 @@ namespace BlazorApp.Components
 {
     public class InitiativeTrackComponent : ComponentBase
     {
-       
+
         [Parameter]
-        public List<Enemy> Enemies { get; set; } 
+        public List<Enemy> Enemies { get; set; }
 
         public Enemy DraggedOver { get; set; }
 
         public Enemy BeingDragged { get; set; }
 
         public string DraggedStyle { get; set; }
-       
 
         public void Shuffle()
         {
             var length = Enemies.Count;
-            var totalToShuffle = length;
-
+            var maxNumber = length;
             Random random = new Random();
             var movedToHistory = new Dictionary<int, Enemy>();
-
-            for (int i = 0; i < length; i++)
+            
+            for (int i = length - 1; i > 0; i--)
             {
-                var moveTo = random.Next(0, length);
-                var moveFrom = random.Next(0, length);
-                if(!movedToHistory.ContainsKey(moveTo))
-                {
-                    var temp = Enemies[moveTo];
-                    var tempTwo = Enemies[i];
-                    Enemies[i] = temp;
-                    Enemies[moveTo] = tempTwo;
-
-                    movedToHistory.Add(moveTo, Enemies[i]);
-                }                
+                var moveOne = random.Next(0, maxNumber);
+                // We don't want the move to be the same as current index
+                // moveTwo = random.Next(0, length);  
+                if(!movedToHistory.ContainsKey(moveOne)) {
+                    var currentEnemy = Enemies[i];
+                    var moveTo = Enemies[moveOne];
+                    Enemies[moveOne] = currentEnemy;
+                    Enemies[i] = moveTo;                   
+                    movedToHistory.Add(moveOne, currentEnemy);
+                }
+                maxNumber--;
             }
         }
         public void HandleDragEnter()
         {
-            
+
         }
 
         public void HandleDragLeave()
         {
-            
+
         }
 
         public string SetDragStyle(Enemy enemy)
         {
-            if(enemy == BeingDragged)
+            if (enemy == BeingDragged)
             {
                 return "dragged";
             }
@@ -65,7 +63,8 @@ namespace BlazorApp.Components
         }
         public void OnBeingDragged(DragEventArgs e, Enemy enemy)
         {
-            BeingDragged = enemy;    
+            e.DataTransfer.DropEffect = "move";
+            BeingDragged = enemy;
         }
 
         public void OnDraggedOver(DragEventArgs e, Enemy enemy)
@@ -76,18 +75,18 @@ namespace BlazorApp.Components
 
         public async Task HandleDrop()
         {
-            var iOne = Enemies.IndexOf(BeingDragged);            
+            var iOne = Enemies.IndexOf(BeingDragged);
             var iTwo = Enemies.IndexOf(DraggedOver);
             // Swap them over
             Enemies[iOne] = DraggedOver;
             Enemies[iTwo] = BeingDragged;
             DraggedOver = null;
             BeingDragged = null;
-            
+
         }
 
         public void Reorder(DragEventArgs e, Enemy enemy)
-        {   
+        {
 
         }
     }
