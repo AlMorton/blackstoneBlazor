59480a997ff3a39f36d36a5a050d4f1c39de369f
Al Morton <morton_al@hotmail.com>
2020-02-29 11:39:12 +0900

    Updated Microsoft.AspNetCore.Blazor.* package references to 3.2.0-preview1.20073.1.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,17 @@
 ﻿using Microsoft.AspNetCore.Blazor.Hosting;
+using System.Threading.Tasks;
 
 namespace BlazorApp
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            await builder.Build().RunAsync();
+            
         }
-
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()            
-                .UseBlazorStartup<Startup>();
-
     }
 }
